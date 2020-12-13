ae243ae2a44a13da924e8f4ec557e6ed60b410ee
Andrei Roman <andreiroman@Andreis-MacBook-Pro.local>
2020-12-13 22:37:38 +0200

    Added event loop scenarios

--- app/event-loop.js
+++ app/event-loop.js
@@ -1,23 +1,44 @@
+const axios = require('axios');
+
 const runForLoopForNTimes = (times) => {
   for (let i = 0; i < times; i++) {
     console.log('C')
   }
 }
 
-eventLoop = async () => {
+const runWhileLoopForNSeconds = (secs) => {
+  let start = Date.now();
+  let now = start;
+
+  while (now - start < (secs * 1000)) {
+    now = Date.now();
+  }
+}
+
+const eventLoopWithForLoop = async () => {
   console.log('A')
   setTimeout(() => console.log('B'), 0);
-  runForLoopForNTimes(3);
+  runForLoopForNTimes(1);
   console.log('D');
 };
 
-// const runWhileLoopForNSeconds = (secs) => {
-//   let start = Date.now();
-//   let now = start;
+const eventLoopWithWhileLoop = async () => {
+  console.log('A')
+  setTimeout(() => console.log('B'), 0);
+  runWhileLoopForNSeconds(1);
+  console.log('D');
+};
 
-//   while (now - start < (secs * 1000)) {
-//     now = Date.now();
-//   }
-// }
+const eventLoopWithApiCall = () => {
+  console.log('A')
+  axios.get('https://jsonplaceholder.typicode.com/users').then(() => console.log('B'));
+  console.log('C');
+}
 
-module.exports = { eventLoop };
+const eventLoopWithAsyncApiCall = async () => {
+  console.log('A')
+  await axios.get('https://jsonplaceholder.typicode.com/users').then(() => console.log('B'));
+  console.log('C');
+}
+
+module.exports = { eventLoopWithForLoop, eventLoopWithWhileLoop, eventLoopWithApiCall, eventLoopWithAsyncApiCall };
